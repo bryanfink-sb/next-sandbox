952ebae6d64dc12ef366bfa7e0dbab1ad5397153
Bryan Fink <bryanfink@Bryans-MacBook-Air.local>
2026-01-05 16:36:27 +0100

    Initial comment

---
+++ src/lib/storyblok.js
@@ -0,0 +1,19 @@
+import { apiPlugin, storyblokInit } from '@storyblok/react/rsc';
+import Page from "@/components/Page";
+import Feature from "@/components/Feature";
+import Grid from "@/components/Grid";
+import Teaser from "@/components/Teaser";
+
+export const getStoryblokApi = storyblokInit({
+  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_CONTENT_API_ACCESS_TOKEN,
+  use: [apiPlugin],
+  apiOptions: {
+    region: 'eu',
+  },
+    components: {
+    page: Page,
+    feature: Feature,
+    grid: Grid,
+    teaser: Teaser
+  },
+});

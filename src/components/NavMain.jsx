a5508ce0787f0fc88c0d7025792e2a7c878781f4
Bryan Fink <bryanfink@Bryans-MacBook-Air.local>
2026-01-15 17:24:05 +0100

    added navigation

---
+++ src/components/NavMain.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { storyblokEditable } from '@storyblok/react'
+import NavMainItem from './nav_main_item'
+
+export default function NavMain({ blok }) {
+  return (
+    <nav {...storyblokEditable(blok)} className="bg-gray-100 p-4">
+      <ul className="flex gap-6">
+        {blok.menu_items?.map((item) => (
+          <NavMainItem key={item._uid} blok={item} />
+        ))}
+      </ul>
+    </nav>
+  )
+}
---
+++ src/components/NavMainItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { storyblokEditable } from '@storyblok/react'
+import Icon from '@iconify/react'
+
+export default function NavMainItem({ blok }) {
+  return (
+    <li {...storyblokEditable(blok)} className="flex items-center gap-2">
+      {blok.icon?.icon && <Icon icon={blok.icon.icon} className="w-5 h-5" />}
+      <a
+        href={blok.link?.cached_url || '#'}
+        className="text-gray-800 hover:text-blue-600 capitalize"
+      >
+        {blok.label}
+      </a>
+    </li>
+  )
+}
